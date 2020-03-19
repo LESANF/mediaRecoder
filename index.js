e764ed04fbf823684d54340949bb0f3ba728d7a8
LESANF <nagong1000@naver.com>
2020-03-20 03:03:52 +0900

    voice END

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const recordBtn = document.getElementById("recordBtn");
+const textCount = document.getElementById("playCount");
 
 const constraints = { audio: true };
+const options = { mimeType: "audio/webm;codecs=opus" };
 
 let streamObj;
 let mediaRecorder;
+const secondsInit = 0;
+let seconds = 0;
+let real;
 
 const handleVoiceData = e => {
   const { data } = event;
+  console.log(data);
   const link = document.createElement("a");
   link.href = URL.createObjectURL(data);
   link.download = "voice.webm";
@@ -15,14 +21,18 @@ const handleVoiceData = e => {
 };
 
 const stopRecording = () => {
+  seconds = 0;
+  clearInterval(real);
+  textCount.innerHTML = "";
   mediaRecorder.stop();
   recordBtn.removeEventListener("click", stopRecording);
-  recordBtn.addEventListener("click", getVoice);
+  recordBtn.addEventListener("click", allowAuth);
+  recordBtn.addEventListener("click", handleCount);
   recordBtn.innerHTML = "start recording";
 };
 
 const startRecording = () => {
-  mediaRecorder = new MediaRecorder(streamObj);
+  mediaRecorder = new MediaRecorder(streamObj, options);
   mediaRecorder.start();
   mediaRecorder.addEventListener("dataavailable", handleVoiceData);
   recordBtn.addEventListener("click", stopRecording);
@@ -31,22 +41,40 @@ const startRecording = () => {
 const getVoice = () => {
   try {
     recordBtn.innerHTML = "stop recording";
-    streamObj = stream;
     startRecording();
   } catch (error) {
     recordBtn.innerHTML = "cant record";
   } finally {
-    recordBtn.removeEventListener("click", getVoice);
+    recordBtn.removeEventListener("click", allowAuth);
+    recordBtn.removeEventListener("click", handleCount);
   }
 };
 
+const getStream = stream => {
+  streamObj = stream;
+};
+
+const allowAuth = () => {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(getStream)
+    .then(getVoice);
+};
+
+const plusNum = () => {
+  seconds += 1;
+  textCount.innerHTML = `recording for : ${seconds}s`;
+};
+
+const handleCount = () => {
+  real = setInterval(plusNum, 1000);
+};
+
 function init() {
-  recordBtn.addEventListener("click", getVoice);
+  recordBtn.addEventListener("click", allowAuth);
+  recordBtn.addEventListener("click", handleCount);
 }
 
 if (recordBtn) {
-  const stream = navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(getVoice);
   init();
 }
